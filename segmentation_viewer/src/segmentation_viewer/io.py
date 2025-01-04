836e1b8a600fb7eec27cdd60bbddfa62ac5c8f3a
Andy Zhang <andyletzhang@gmail.com>
2025-01-03 22:06:50 -0600

    refactored tiff loading part 2

--- segmentation_viewer/src/segmentation_viewer/io.py
+++ segmentation_viewer/src/segmentation_viewer/io.py
@@ -1,41 +1,51 @@
 
 from nd2 import ND2File
-from segmentation_tools.io import read_nd2, nd2_zstack, nd2_frame, read_nd2_shape
+from segmentation_tools.io import read_nd2, read_nd2_shape, read_tif, read_tif_shape
+from tifffile import TiffFile
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QFormLayout, QLineEdit, QPushButton, QHBoxLayout
 from pathlib import Path
 import numpy as np
 
-def read_nd2_file(file_path, progress_bar=None, **progress_kwargs):
+def read_image_file(file_path, progress_bar=None, **progress_kwargs):
     if progress_bar is None:
         progress_bar = lambda x, **kwargs: x
-    with ND2File(file_path) as file:
+    if file_path.endswith('.nd2'):
+        file=ND2File(file_path)
         shape=read_nd2_shape(file) # (T, Z, C, Y, X)
-        shape_dialog = ND2ShapeDialog(shape) # Prompt user to select ranges to import for each dimension
-        if shape_dialog.exec_() == QDialog.Accepted:
-            try:
-                t_bounds, z_bounds, c_bounds=shape_dialog.get_selected_ranges()
-            except ValueError as e:
-                print(f"Error: {e}")
-        else:
-            return None
-        
-        nd2_file=read_nd2(file) # Load the ND2 file
-        sliced=nd2_file[np.ix_(t_bounds, z_bounds)] # index the T and Z dimensions
-        img_shape=(shape[3], shape[4], len(c_bounds)) # output image shape (Y, X, C)
+        placeholder=read_nd2(file) # Load the ND2 file
+    else:
+        file=TiffFile(file_path)
+        shape, order=read_tif_shape(file)
+        axes_map = {axis: i for i, axis in enumerate(reversed(order))}
+        shape=tuple(shape[axes_map[axis]] for axis in 'TZCYX')
+        placeholder=read_tif(file) # Load the TIFF file
 
-        # Assuming `sliced_array` is the array of functions
-        result_array = np.empty((*sliced.shape, *img_shape), dtype=np.int16)  # Initialize output array
+    shape_dialog = ShapeDialog(shape) # Prompt user to select ranges to import for each dimension
+    if shape_dialog.exec_() == QDialog.Accepted:
+        try:
+            t_bounds, z_bounds, c_bounds=shape_dialog.get_selected_ranges()
+        except ValueError as e:
+            print(f"Error: {e}")
+    else:
+        return None
+    
+    sliced=placeholder[np.ix_(t_bounds, z_bounds)] # index the T and Z dimensions
+    img_shape=(shape[3], shape[4], len(c_bounds)) # output image shape (Y, X, C)
 
-        # Read the data from the ND2 file
-        for idx, func in progress_bar(np.ndenumerate(sliced), length=sliced.size, **progress_kwargs):
-            result_array[idx] = func()[c_bounds].transpose(1,2,0)  # Call the function and store the result
-        return result_array
-        
-class ND2ShapeDialog(QDialog):
-    def __init__(self, nd2_shape, parent=None):
+    # Assuming `sliced_array` is the array of functions
+    result_array = np.empty((*sliced.shape, *img_shape), dtype=np.uint16)  # Initialize output array
+
+    # Read the data from the file
+    for idx, func in progress_bar(np.ndenumerate(sliced), length=sliced.size, **progress_kwargs):
+        result_array[idx] = func()[c_bounds].transpose(1,2,0)  # Call the function and store the result
+    file.close()
+    return result_array
+
+class ShapeDialog(QDialog):
+    def __init__(self, shape, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Load ND2 file...")
-        self.nd2_shape = nd2_shape
+        self.setWindowTitle("Select dimension ranges")
+        self.shape = shape
         self.setMinimumWidth(300)
         self.subset_ranges = {}
         
@@ -56,7 +66,7 @@ class ND2ShapeDialog(QDialog):
             self.line_edits[dim] = range_edit
             
             # Create label with range information
-            label = f"{dim} (0 - {nd2_shape[i] - 1})"
+            label = f"{dim} (0 - {self.shape[i] - 1})"
             
             # Add row to form layout
             form_layout.addRow(label, range_edit)
@@ -118,7 +128,7 @@ class ND2ShapeDialog(QDialog):
         try:
             for dim, line_edit in self.line_edits.items():
                 dim_idx = ['T', 'Z', 'C'].index(dim)
-                max_value = self.nd2_shape[dim_idx]
+                max_value = self.shape[dim_idx]
                 indices = self.parse_range(line_edit.text(), max_value)
                 ranges[dim] = indices
             
